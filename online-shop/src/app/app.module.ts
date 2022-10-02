99b9e3b08a5c48cb002c0106cbd66da1a15ea137
Ramy Ibrahim <r.ibrahim@prosylab.com>
2022-10-02 21:55:28 +0200

    init

---
+++ online-shop/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app-component/app.component';
+import { ImageComponentComponent } from './components/image-component/image-component.component';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/layouts/header/header.component';
+import { FooterComponent } from './components/layouts/footer/footer.component';
+import { HomeComponent } from './components/views/home/home.component';
+import { ShopComponent } from './components/views/shop/shop.component';
+import { DetailComponent } from './components/views/detail/detail.component';
+import { CartComponent } from './components/views/cart/cart.component';
+import { CheckoutComponent } from './components/views/checkout/checkout.component';
+import { ContactComponent } from './components/views/contact/contact.component';
+import { CategoriesComponent } from './components/sub-components/categories/categories.component';
+import { CategoryComponent } from './components/sub-components/category/category.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ImageComponentComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ShopComponent,
+    DetailComponent,
+    CartComponent,
+    CheckoutComponent,
+    ContactComponent,
+    CategoriesComponent,
+    CategoryComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
